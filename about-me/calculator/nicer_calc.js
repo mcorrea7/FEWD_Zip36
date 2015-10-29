07a9dcdf019c3effba5582fcac3079ec45d1e17b
mcorrea7 <mcorrea@alumni.risd.edu>
2015-10-28 18:14:39 -0700

    temp converter js

---
+++ about-me/calculator/nicer_calc.js
@@ -0,0 +1,39 @@
+// Structure --------------------------------------------------------
+var tempF = document.querySelector('.temp-f');
+var tempC = document.querySelector('.temp-c');
+var compareC = document.querySelector('.compareC')
+var compareF = document.querySelector('.compareF')
+
+// Events -----------------------------------------------------------
+compareC.addEventListener('click', calcFtoC);
+compareF.addEventListener('click', calcCtoF);
+
+
+
+// Event Handlers ---------------------------------------------------
+function calcFahrenheitToCelcius(tempC) {
+  var tempF = (tempC - 32) * 5/9;
+  return tempF;
+}
+
+function calcFtoC(event) {
+  if (tempF.value.length > 0) {
+    tempC.value = calcFahrenheitToCelcius(tempF.value)
+  } else {
+    tempC.value = '';
+  }
+}
+
+
+function calcCelciusToFahrenheit(tempF) {
+  var tempC = tempF * 9/5 + 32;
+  return tempC;
+}
+
+function calcCtoF(event) {
+  if (tempC.value.length > 0) {
+    tempF.value = calcCelciusToFahrenheit(tempC.value)
+  } else {
+    tempF.value = '';
+  }
+}

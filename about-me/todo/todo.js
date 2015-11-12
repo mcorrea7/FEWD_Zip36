73cb829ee23de2bd75a5652e28eac6b5956ef859
mcorrea7 <mcorrea@alumni.risd.edu>
2015-11-11 19:49:04 -0800

    edited

--- about-me/todo/todo.js
+++ about-me/todo/todo.js
@@ -4,10 +4,10 @@ var task   = document.querySelector(".task");
 var date   = document.querySelector(".date");
 var button = document.querySelector("button");
 var list   = document.querySelector(".list");
-var check  = document.querySelector(".check");
 
 console.log(task);
 console.log(date);
+
 
 
 //Events
@@ -15,24 +15,24 @@ form.addEventListener("submit", todo);
 
 //Event Handlers
 function todo(a) {
-	a.preventDefault();									// prevents default form behaviours
+	a.preventDefault();										// prevents default form behaviours
 
 	// Define values of task & date inputs
 	var taskValue = task.value;
 	var dateValue = date.value;
 
 	// Create Elements
-	var checkbox = document.createElement("input");		// creates a checkbox
-	var newTask = document.createElement("LI");			// creates the new task with check box and text
-	var textArea = document.createElement("span");		// creates a span for the task and date
+	var checkbox = document.createElement("input");			// creates a checkbox
+	var newTask = document.createElement("LI");				// creates the new task with check box and text
+	var textArea = document.createElement("span");			// creates a span for the task and date
 
 	// Decorate Elements
-	checkbox.setAttribute("type", "checkbox");			// define the type of box
-	textArea.textContent = taskValue + " " + dateValue;	// defines the text in the new element
+	checkbox.setAttribute("type", "checkbox");				// define the type of box
+	textArea.textContent = taskValue + " " + dateValue;		// defines the text in the new element
 
 	// Append Elements
-	list.appendChild(newTask);							// adds newTask to the ul
-	newTask.appendChild(checkbox);						// adds the checkbox to newTask
-	newTask.appendChild(textArea);						// adds all text to newTask
+	list.appendChild(newTask);								// adds newTask to the ul
+	newTask.appendChild(checkbox);							// adds the checkbox to newTask
+	newTask.appendChild(textArea);							// adds all text to newTask
 
 }
